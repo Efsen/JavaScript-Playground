90d26484ca86d398e58a8c36746cd18561355a3b
Efsen <80422935+Efsen@users.noreply.github.com>
2022-04-14 22:18:20 +0300

    Add files via upload

---
+++ javascript playground/01-hello-word.js
@@ -0,0 +1,10 @@
+//This is my first Js code
+
+//Console 
+console.log('Hello World');
+
+//Document
+//document.write('Hello World');
+
+// Alert
+//alert('Hello World');
---
+++ javascript playground/02-let-conts-var.js
@@ -0,0 +1,46 @@
+//var
+//var firstName = "Kaan"
+//console.log(firstName);
+
+/*---------------------------- */
+//let undefined
+let serverName;
+console.log(serverName)
+
+//let variable assignment
+serverName = "www.kaanefsen.com";
+console.log(serverName);
+
+//let variable assignment
+let password = "123456";
+console.log(password);
+
+//let
+/*
+    Wrong Way
+    console.log(fullName);
+    let fullName = ("Kaan EFSEN");
+
+ */
+let fullName = ("Kaan EFSEN");
+console.log(fullName);
+
+//merge and add
+fullName += " New information"
+console.log(fullName);
+fullName = fullName + " New";
+console.log(fullName)
+
+/*--------------------------- */
+//const assigment didnt work
+
+//const serverPassword; wrong
+
+const SERVER_PASSWORD = "AkldalAD1861";
+console.log(SERVER_PASSWORD);
+
+//SERVER_PASSWORD = "123456";
+//console.log(SERVER_PASSWORD);
+
+
+
