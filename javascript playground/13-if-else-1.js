810b7fbf29a55458ab32642ca1b9b8ced0d527a5
Kaan EFSEN <80422935+Efsen@users.noreply.github.com>
2022-04-21 21:42:47 +0300

    Add files via upload

---
+++ javascript playground/13-if-else-1.js
@@ -0,0 +1,44 @@
+let price = 100
+let user = "Name"
+
+
+//eşitse
+
+console.log("Price: ==" , price == 1)
+console.log("Price: ==" , price == 100)
+
+// ===
+console.log("Price: ===" , price === 1)
+console.log("Price: ===" , price === 100)
+
+// !=
+console.log("Price: !=" , price != 1)
+console.log("Price: !=" , price != 100)
+
+// <
+
+console.log("Price: < 1" , price < 1)
+console.log("Price: < 1000" , price < 1000)
+console.log("Price: <= 1000" , price <= 1000)
+
+// >
+
+console.log("Price: > 1" , price > 1)
+console.log("Price: > 1000" , price > 1000)
+console.log("Price: >= 1000" , price >= 1000)
+
+// &&
+
+console.log(price > 0 && user != "Name")
+
+// ||
+
+console.log(price > 0 || user != "Name")
+
+
+
+
+
+
+
+
---
+++ javascript playground/14-if-else-2.js
@@ -0,0 +1,8 @@
+let username = prompt("What's your name :)")
+
+if (username.length > 0) {
+    console.log(`Your User Name ${username}`)
+}
+else{
+    console.log("No Entry")
+}
