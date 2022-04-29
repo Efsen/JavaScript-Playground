6246608bad563dbfa570a28747c5426e0da95967
Kaan EFSEN <80422935+Efsen@users.noreply.github.com>
2022-04-29 22:12:39 +0300

    Add files via upload

---
+++ javascript playground/23-function-3.js
@@ -0,0 +1,37 @@
+
+    let counter = 0 
+    let counterdom = document.querySelector('#counter')
+    let increasedom = document.querySelector('#increase')
+    let decreasedom = document.querySelector('#decrease')
+
+    counterdom.innerHTML = counter
+
+    increasedom.addEventListener("click",clickevent )
+    decreasedom.addEventListener("click",clickevent )
+
+   /* function clickevent(){
+        console.log(this.id)
+        if(this.id == "increase"){
+            counterdom.innerHTML = counter +=1 
+        }
+        if(this.id == "decrease" && counter > 0){
+            counterdom.innerHTML = counter -=1 
+        }
+    }*/
+
+    /*function clickevent(){
+        console.log(this.id)
+        if(this.id == "increase"){
+            counterdom.innerHTML = counter +=1 
+        }
+        else{
+            counterdom.innerHTML = counter -=1 
+        }
+    }*/
+
+    function clickevent(){
+        console.log(this.id)
+        this.id == "increase" ? counter += 1 : counter -= 1 && counter > 0
+        counterdom.innerHTML = counter
+    }
+
